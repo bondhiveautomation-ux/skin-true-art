faa3b7acb474696d4138d33cbfd4b125cf0ef254
bondhiveautomation-ux <bondhiveautomation@gmail.com>
2026-01-31 09:26:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+allowedHosts: [".emergentagent.com"],
+
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
